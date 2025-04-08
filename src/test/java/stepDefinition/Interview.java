ee2ac2c1f693c7f6604a37d4c504248d2779ca4f
Saumya <singhsaumyait034@gmail.com>
2025-04-08 07:54:16 +0530

    Interview Practice

--- src/test/java/stepDefinition/Interview.java
+++ src/test/java/stepDefinition/Interview.java
@@ -1,7 +1,11 @@
 package stepDefinition;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.Before;
 import io.restassured.response.Response;
+import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 
@@ -27,5 +31,31 @@ public class Interview {
            System.out.println("getting error while"+response.getStatusCode());
        }
        tokenExpiryTime = System.currentTimeMillis() + (30 * 60 * 1000);
+    }
+
+    @Test
+    public void testRun() throws JsonProcessingException {
+        Response response = given().when().get("");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node =  mapper.readTree(response.getBody().asString());
+        if(node.has("data")){
+            JsonNode data = node.get("data");
+        }
+    }
+
+    @Test
+    public void test() throws JsonProcessingException {
+        Response response = given().when().get("");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(response.getBody().asString());
+        JsonNode data = root.get("data");
+        if(data.isTextual()){
+
+        }else if(data.isArray()){
+
+        }else if(data.isObject()){
+
+        }
+
     }
 }
