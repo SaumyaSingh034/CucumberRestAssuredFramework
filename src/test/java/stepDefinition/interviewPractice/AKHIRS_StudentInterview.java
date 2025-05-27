e8f687f0cfb2b93a48215561bd952d815ba535f0
Saumya <singhsaumyait034@gmail.com>
2025-05-27 11:39:50 +0530

    AKHIRS - Student Interview

--- src/test/java/stepDefinition/interviewPractice/AKHIRS_StudentInterview.java
+++ src/test/java/stepDefinition/interviewPractice/AKHIRS_StudentInterview.java
@@ -4,23 +4,46 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.cienvironment.internal.com.eclipsesource.json.JsonObject;
+import io.cucumber.java.eo.Se;
 import io.restassured.path.json.JsonPath;
 import payloadBuilder.Payload1;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class AKHIRS_StudentInterview {
     public static void main(String[] args) throws JsonProcessingException {
         String response = Payload1.payload2;
-        ObjectMapper obj = new ObjectMapper();
-        JsonNode root = obj.readTree(response);
-        JsonNode students = root.path("students");
-        System.out.println(students);
-        for(JsonNode student : students){
-            if("New Delhi".equalsIgnoreCase(student.get("place").asText())){
-                System.out.println(student.get("name").asText());
+//        ObjectMapper obj = new ObjectMapper();
+//        JsonNode root = obj.readTree(response);
+//        JsonNode students = root.path("students");
+//        System.out.println(students);
+//        for(JsonNode student : students){
+//            if("New Delhi".equalsIgnoreCase(student.get("place").asText())){
+//                System.out.println(student.get("name").asText());
+//            }
+//        }
+
+        JsonPath js = new JsonPath(response);
+        int size = js.get("students.size()");
+        System.out.println(size);
+        int count = 0;
+        Set<String> list = new HashSet<>();
+
+        for(int i=0;i<size;i++){
+            String section = js.getString("students["+i+"].section");
+            if("A".equalsIgnoreCase(section)){
+                System.out.println(js.getString("students["+i+"].name"));
             }
+            if("New Delhi".equalsIgnoreCase(js.getString("students["+i+"].place")))
+                count++;
+            list.add(js.getString("students["+i+"].place"));
+
+
         }
+        System.out.println("Number of students from New Delhi: "+count);
+        System.out.println(list);
 
     }
 }
